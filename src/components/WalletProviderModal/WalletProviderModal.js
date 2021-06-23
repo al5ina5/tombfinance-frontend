bc69412520d863e2c3ce4a18ca9733a52029e075
Sebastian Alsina <alsinas@me.com>
2021-06-23 12:44:50 -0400

    MaxWidth 300PX

--- src/components/WalletProviderModal/WalletProviderModal.js
+++ src/components/WalletProviderModal/WalletProviderModal.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     width: '95%',
-    maxWidth: '400px',
+    maxWidth: '300px',
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(4),

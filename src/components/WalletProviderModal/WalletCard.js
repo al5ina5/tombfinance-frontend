805f972449e68b55bf9ce9c27900b03705b7a9c2
Sebastian Alsina <alsinas@me.com>
2021-06-23 12:27:15 -0400

    Formatted Code

--- src/components/WalletProviderModal/WalletCard.js
+++ src/components/WalletProviderModal/WalletCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
 const WalletCard = ({ icon, onConnect, title }) => (
-  <button type="button" onClick={onConnect} style={{display:'flex', alignItems:'center', outline: 'none', border:'none', width:'100%'}}>
-    <span style={{marginRight: '1rem'}}>{icon}</span>
+  <button
+    type="button"
+    onClick={onConnect}
+    style={{ display: 'flex', alignItems: 'center', outline: 'none', border: 'none', width: '100%' }}
+  >
+    <span style={{ marginRight: '1rem' }}>{icon}</span>
     <span>{title}</span>
   </button>
-  // <ListItem button onClick={onConnect}>
-  //   <ListItemIcon>{icon}</ListItemIcon>
-  //   <ListItemText primary={title} />
-  // </ListItem>
 );
 
 export default WalletCard;
